aee489e7eeaa2295e965c5a6d7a596b8c3a82895
a7md3adl <a7md.3adl@yahoo.com>
2021-04-01 01:24:57 +0200

    first upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace NetworkConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ConsoleWindow = new System.Windows.Forms.TextBox();
+            this.BroadcastTimer = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BroadcastBtn = new System.Windows.Forms.ToolStripButton();
+            this.MyIPLbl = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ConsoleWindow
+            // 
+            this.ConsoleWindow.BackColor = System.Drawing.Color.White;
+            this.ConsoleWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConsoleWindow.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConsoleWindow.Location = new System.Drawing.Point(0, 0);
+            this.ConsoleWindow.Multiline = true;
+            this.ConsoleWindow.Name = "ConsoleWindow";
+            this.ConsoleWindow.ReadOnly = true;
+            this.ConsoleWindow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ConsoleWindow.Size = new System.Drawing.Size(800, 450);
+            this.ConsoleWindow.TabIndex = 0;
+            // 
+            // BroadcastTimer
+            // 
+            this.BroadcastTimer.Tick += new System.EventHandler(this.BroadcastTimer_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BroadcastBtn,
+            this.MyIPLbl});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BroadcastBtn
+            // 
+            this.BroadcastBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BroadcastBtn.Image = global::NetworkConsole.Properties.Resources.network;
+            this.BroadcastBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BroadcastBtn.Name = "BroadcastBtn";
+            this.BroadcastBtn.Size = new System.Drawing.Size(23, 22);
+            this.BroadcastBtn.Text = "Broadcast";
+            this.BroadcastBtn.Click += new System.EventHandler(this.BroadcastBtn_Click);
+            // 
+            // MyIPLbl
+            // 
+            this.MyIPLbl.Name = "MyIPLbl";
+            this.MyIPLbl.Size = new System.Drawing.Size(34, 22);
+            this.MyIPLbl.Text = "MyIP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.ConsoleWindow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyRetailerMobileConsole";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConsoleWindow;
+        private System.Windows.Forms.Timer BroadcastTimer;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BroadcastBtn;
+        private System.Windows.Forms.ToolStripLabel MyIPLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkConsole
+{
+    public partial class Form1 : Form
+    {
+        UdpClient receiver = new UdpClient(48762);
+
+        public Form1()
+        {
+            InitializeComponent();
+            UDPsender.EnableBroadcast = true;
+            UDPsender.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            UDPsender.ExclusiveAddressUse = false;
+            receiver.BeginReceive(DataReceived, receiver);                //}
+            MyIPLbl.Text = MyIPAddress;
+            BroadcastBtn.PerformClick();
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            UdpClient c = (UdpClient)ar.AsyncState;
+            if (c == null) return;
+
+            try
+            {
+
+
+                IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 48762);
+                if (receivedIpEndPoint == null) return;
+                Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+
+                // Convert data to ASCII and print in console
+                string receivedText = Encoding.UTF8.GetString(receivedBytes);
+                //Console.WriteLine(receivedText);
+                if (receivedText.IndexOf("^") > 0)
+                {
+                    string[] arr = receivedText.Split('^');
+                    if (arr.Length > 0)
+                    {
+                        //if (arr[0] == "MyRetailer Broadcast Server")
+                        //{
+                        //    System.Console.Beep();
+
+                        //    var builer = new StringBuilder();
+                        //    builer.Append(string.Format(DateTime.Now.ToString("hh:mm:ss ") + "\t{0}                            : {1}", arr[0], arr[1]));
+                        //    builer.Append(Environment.NewLine);
+                        //    Rt += builer.ToString(); this.Invoke(new EventHandler(datarecieved));
+
+                        //}
+                        //else
+                        {
+                            System.Console.Beep();
+
+                            var builer = new StringBuilder();
+                            builer.Append(string.Format(DateTime.Now.ToString("hh:mm:ss ") + "\t{0}                            : {1}", arr[0], arr[1]));
+                            builer.Append(Environment.NewLine);
+                            Rt += builer.ToString();
+                            this.Invoke(new EventHandler(datarecieved));
+
+                        }
+
+                    }
+
+                }
+                c.BeginReceive(DataReceived, ar.AsyncState);
+            }
+            catch (Exception ex) { }
+
+        }
+        string Rt;
+        private void datarecieved(object sender, EventArgs e)
+        {
+            
+            
+            ConsoleWindow.AppendText(Rt);
+            Rt = "";
+            ConsoleWindow.ScrollToCaret();
+            //ConsoleWindow.SelectionLength = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            receiver.Close();
+            receiver.Dispose();
+        }
+        public static UdpClient UDPsender = new UdpClient();
+
+        private void BroadcastTimer_Tick(object sender, EventArgs e)
+        {
+            IPEndPoint broadcastAddress = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 48769);
+            
+            UDPsender.Send(Encoding.UTF8.GetBytes("MyRetailer Broadcast Server^" + MyIPAddress), Encoding.UTF8.GetBytes("MyRetailer Broadcast Server^" + MyIPAddress).Length, broadcastAddress);
+
+        }
+        private string _myIP = "";
+        private string MyIPAddress
+        {
+            get
+            {
+                if (_myIP == "") _myIP = DisplayIPAddresses();
+                return _myIP;
+            }
+        }
+        public static string DisplayIPAddresses()
+        {
+            string returnAddress = String.Empty;
+
+            // Get a list of all network interfaces (usually one per network card, dialup, and VPN connection)
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface network in networkInterfaces)
+            {
+                // Read the IP configuration for each network
+                IPInterfaceProperties properties = network.GetIPProperties();
+
+                if (network.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                       network.OperationalStatus == OperationalStatus.Up &&
+                       !network.Description.ToLower().Contains("virtual") &&
+                       !network.Description.ToLower().Contains("pseudo"))
+                {
+                    // Each network interface may have multiple IP addresses
+                    foreach (IPAddressInformation address in properties.UnicastAddresses)
+                    {
+                        // We're only interested in IPv4 addresses for now
+                        if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                            continue;
+
+                        // Ignore loopback addresses (e.g., 127.0.0.1)
+                        if (IPAddress.IsLoopback(address.Address))
+                            continue;
+
+                        returnAddress = address.Address.ToString();
+                        //Console.WriteLine(address.Address.ToString() + " (" + network.Name + " - " + network.Description + ")");
+                    }
+                }
+                else
+                {
+                    if (network.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 &&
+                       network.OperationalStatus == OperationalStatus.Up &&
+                       !network.Description.ToLower().Contains("virtual") &&
+                       !network.Description.ToLower().Contains("pseudo"))
+                    {
+                        // Each network interface may have multiple IP addresses
+                        foreach (IPAddressInformation address in properties.UnicastAddresses)
+                        {
+                            // We're only interested in IPv4 addresses for now
+                            if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                                continue;
+
+                            // Ignore loopback addresses (e.g., 127.0.0.1)
+                            if (IPAddress.IsLoopback(address.Address))
+                                continue;
+
+                            returnAddress = address.Address.ToString();
+                            //Console.WriteLine(address.Address.ToString() + " (" + network.Name + " - " + network.Description + ")");
+                        }
+                    }
+                }
+            }
+
+            return returnAddress;
+        }
+
+        private void BroadcastBtn_Click(object sender, EventArgs e)
+        {
+            BroadcastBtn.Checked = !BroadcastBtn.Checked;
+            if(BroadcastBtn.Checked)
+            {
+                BroadcastTimer.Interval = 2000;
+                BroadcastTimer.Enabled = true;
+            }
+            else
+            {
+                BroadcastTimer.Enabled = false;
+
+            }
+        }
+    }
+}
